210d64a3890bbbe6e0e49b3a769e0f86a0704581
matt578 <mattandraedajao20@gmail.com>
2026-05-26 15:55:44 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$user = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$pass = $_ENV['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
+$db   = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$port = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
+
+$conn = new mysqli(
+    $host,
+    $user,
+    $pass,
+    $db,
+    (int)$port
+);
+
+if ($conn->connect_error){
+    die($conn->connect_error);
+}
+
+$conn->set_charset('utf8mb4');
+?>
