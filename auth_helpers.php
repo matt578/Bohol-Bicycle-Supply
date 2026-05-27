f7aa3d5edadb84305275679fe188dc5721743aeb
matt578 <mattandraedajao20@gmail.com>
2026-05-28 01:02:22 +0800

    Update auth_helpers.php

--- auth_helpers.php
+++ auth_helpers.php
@@ -21,7 +21,7 @@ function completeLogin(array $row, mysqli $conn, string $method = 'email') {
     $_SESSION['email'] = $row['email'] ?? '';
     $_SESSION['login_method'] = $method;
 
-    header('Location: Dashboard.php');
+    header('Location: dashboard.php');
     exit();
 }
 
